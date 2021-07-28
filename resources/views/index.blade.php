677d71c61d3fb123f03ecb07271ccb5a0cd594a2
Aknurdaulet <aknrdlt@gmail.com>
2021-07-28 18:06:46 +0600

    ff

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -131,33 +131,7 @@
     <section class="statistics" data-aos="fade-up">
       <div class="container">
         <h1 class="title">Nunc sed blandit libero volutpat</h1>
-        <!-- <div class="inner-blocks" id="counter">
-          <div class="statistics-block">
-            <img src="/static/icons/cafe.svg" alt="" class="statistics-icon" />
-            <div class="statistics-data">
-              <h2 class="counter-value" data-count="{{$data -> coworkers}}">0</h2>
-              <span>Заведений уже с нами</span>
-            </div>
-          </div>
-          <div class="statistics-block">
-            <img src="/static/icons/users.svg" alt="" class="statistics-icon" />
-            <div class="statistics-data">
-              <h2 class="counter-value" data-count="{{$data -> users}}">900</h2>
-              <span>Человек пользуются Order Eat</span>
-            </div>
-          </div>
-          <div class="statistics-block">
-            <img
-              src="/static/icons/waiter.svg"
-              alt=""
-              class="statistics-icon"
-            />
-            <div class="statistics-data">
-              <h2 class="counter-value" data-count="{{$data -> staff}}">200</h2>
-              <span>Официантов пользуются Order Eat</span>
-            </div>
-          </div>
-        </div> -->
+     
       </div>
     </section>
     <section class="news" data-aos="fade-right">
