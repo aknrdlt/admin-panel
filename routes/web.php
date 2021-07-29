3388f3e3c775018db8e8f15d00ba66e184f68edc
Aknurdaulet <aknrdlt@gmail.com>
2021-07-29 13:51:51 +0600

    sad

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,8 @@ Route::group(['prefix' => 'admin'], function () {
 Route::get('info', function () {
     phpinfo();
 });
-Route::get('/linkstorage', function () {
-    Artisan::call('storage:link');
+Route::get("/storage-link", function () {
+    $targetFolder = storage_path("app/public");
+    $linkFolder = $_SERVER['DOCUMENT_ROOT'] . '/storage';
+    symlink($targetFolder, $linkFolder);
 });
