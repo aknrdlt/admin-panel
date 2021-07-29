ed52e8152d7ceb24eea5ec4365f73b32e72e0bc3
Aknurdaulet <aknrdlt@gmail.com>
2021-07-29 12:52:27 +0600

    sdfds

--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -1,27 +1,27 @@
 <div class="inner-blocks" id="counter">
           <div class="statistics-block">
-            <img src="/static/icons/cafe.svg" alt="" class="statistics-icon" />
+            <img src="{{asset($data->image_coworkers)}}" alt="" class="statistics-icon" />
             <div class="statistics-data">
               <h2 class="counter-value" data-count="{{$data -> coworkers}}">0</h2>
-              <span>Заведений уже с нами</span>
+              <span>{{$data -> text_coworkers}}</span>
             </div>
           </div>
           <div class="statistics-block">
-            <img src="/static/icons/users.svg" alt="" class="statistics-icon" />
+            <img src="{{asset($data->image_users)}}" alt="" class="statistics-icon" />
             <div class="statistics-data">
               <h2 class="counter-value" data-count="{{$data -> users}}">900</h2>
-              <span>Человек пользуются Order Eat</span>
+              <span>{{$data -> text_users}}</span>
             </div>
           </div>
           <div class="statistics-block">
             <img
-              src="/static/icons/waiter.svg"
+              src="{{asset($data->image_staff)}}"
               alt=""
               class="statistics-icon"
             />
             <div class="statistics-data">
               <h2 class="counter-value" data-count="{{$data -> staff}}">200</h2>
-              <span>Официантов пользуются Order Eat</span>
+              <span>{{$data -> text_staff}}</span>
             </div>
           </div>
         </div>
