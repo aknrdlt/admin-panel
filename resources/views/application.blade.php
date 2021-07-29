7db1347bd177ec7cc1ec881938249a286279abe0
Aknurdaulet <aknrdlt@gmail.com>
2021-07-29 13:09:40 +0600

    dsfdsf

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -10,17 +10,17 @@
         />
         <div class="input-row">
           <select name="role" id="iam" class="half-sized">
-            <option selected="selected" value="owner" selected>Я владелец</option>
-            <option value="client">Я пользователь</option>
-            <option value="client">Я официант</option>
+            <option selected="selected" value="Owner" selected>Я владелец</option>
+            <option value="User">Я пользователь</option>
+            <option value="Worker">Я официант</option>
           </select>
           <select name="goal" id="purpose" class="half-sized">
-            <option selected="selected" value="purpose">
+            <option selected="selected" value="Goal 0">
               Цель сотрудничества
             </option>
-            <option value="purpose1">Цель #1</option>
-            <option value="purpose2">Цель #2</option>
-            <option value="purpose3">Цель #3</option>
+            <option value="Goal 1">Цель #1</option>
+            <option value="Goal 2">Цель #2</option>
+            <option value="Goal 3">Цель #3</option>
           </select>
         </div>
         <div class="input-row">
