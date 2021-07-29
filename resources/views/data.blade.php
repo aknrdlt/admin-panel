3e0f7ecf20d39f85c8e035bda367587f70835649
Aknurdaulet <aknrdlt@gmail.com>
2021-07-29 18:25:59 +0600

    sddsf

--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -2,15 +2,15 @@
           <div class="statistics-block">
             <img src="/static/icons/cafe.svg" alt="" class="statistics-icon" />
             <div class="statistics-data">
-              <h2 class="counter-value" data-count="{{$data -> coworkers}}">0</h2>
-              <span>{{$data -> text_coworkers}}</span>
+              <h2 class="counter-value" data-count="20">0</h2>
+              <span>$data -> text_coworkers</span>
             </div>
           </div>
           <div class="statistics-block">
             <img src="/static/icons/users.svg" alt="" class="statistics-icon" />
             <div class="statistics-data">
-              <h2 class="counter-value" data-count="{{$data -> users}}">900</h2>
-              <span>{{$data -> text_users}}</span>
+              <h2 class="counter-value" data-count="1000">900</h2>
+              <span>$data -> text_users</span>
             </div>
           </div>
           <div class="statistics-block">
@@ -20,8 +20,8 @@
               class="statistics-icon"
             />
             <div class="statistics-data">
-              <h2 class="counter-value" data-count="{{$data -> staff}}">200</h2>
-              <span>{{$data -> text_staff}}</span>
+              <h2 class="counter-value" data-count="400">200</h2>
+              <span>$data -> text_staff</span>
             </div>
           </div>
         </div>
