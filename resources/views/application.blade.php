f785de6bcaa6d945efb4c3e0aed74a3080dbba1b
Aknurdaulet <aknrdlt@gmail.com>
2021-07-29 15:07:40 +0600

    dass

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -1,7 +1,6 @@
 <img src="/static/images/form-image.png" alt="" class="form-image" />
       <div class="form">
         <form  method="POST" action="">
-        <?php echo $restaurant_name?>
         <h1 class="title">Оставьте заявку и мы обязательно свяжемся с вами!</h1>
         <input
           type="text"
@@ -52,6 +51,6 @@
           $username = $_POST["username"];
           $phone = $_POST["phone"];
 
-          
+          echo $restaurant_name
         }
       ?>
