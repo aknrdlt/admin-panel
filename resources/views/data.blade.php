5ef14435d20f328318c98d594dc402fce12e4f24
Aknurdaulet <aknrdlt@gmail.com>
2021-07-30 10:42:55 +0600

    dfds

--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -1,17 +1,13 @@
 <div class="inner-blocks" id="counter">
           <div class="statistics-block">
-            <img src="{{$file = Input::file(Voyager::image($data -> image_users));
-                        $filename = '...';
-                        Image::make($file->getRealPath())->resize('200','200')->save($filename);}}" alt="" class="statistics-icon" />
+            <img src="{{Image::make(Voyager::image($data -> image_coworkers))}}" alt="" class="statistics-icon" />
             <div class="statistics-data">
               <h2 class="counter-value" data-count="20">0</h2>
               <span>{{$data -> text_coworkers}}</span>
             </div>
           </div>
           <div class="statistics-block">
-            <img src="{{$file = Input::file(Voyager::image($data -> image_users));
-                        $filename = '...';
-                        Image::make($file->getRealPath())->resize('200','200')->save($filename);}}" alt="" class="statistics-icon" />
+            <img src="{{Imageick::readImageFile(Voyager::image($data -> image_users))}}" alt="" class="statistics-icon" />
             <div class="statistics-data">
               <h2 class="counter-value" data-count="{{$data -> users}}">900</h2>
               <span>{{$data -> text_users}}</span>
@@ -19,9 +15,7 @@
           </div>
           <div class="statistics-block">
             <img
-              src="{{$file = Input::file(Voyager::image($data -> image_users));
-                      $filename = '...';
-                      Image::make($file->getRealPath())->resize('200','200')->save($filename);}}"
+              src="{{Imageick::readImageFile(Voyager::image($data -> image_staff))}}"
               alt=""
               class="statistics-icon"
             />
