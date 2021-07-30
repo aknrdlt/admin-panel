c1bf4e97949c3ea5e4226c835c9553ba0efcd751
Aknurdaulet <aknrdlt@gmail.com>
2021-07-30 10:15:21 +0600

    dfs

--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -1,13 +1,13 @@
 <div class="inner-blocks" id="counter">
           <div class="statistics-block">
-            <img src="{{Voyager::image($data -> image_coworkers)}}" alt="" class="statistics-icon" />
+            <img src="{{Image::make(Voyager::image($data -> image_coworkers))}}" alt="" class="statistics-icon" />
             <div class="statistics-data">
               <h2 class="counter-value" data-count="20">0</h2>
               <span>{{$data -> text_coworkers}}</span>
             </div>
           </div>
           <div class="statistics-block">
-            <img src="{{Voyager::image($data -> image_users)}}" alt="" class="statistics-icon" />
+            <img src="{{Image::make(Voyager::image($data -> image_users))}}" alt="" class="statistics-icon" />
             <div class="statistics-data">
               <h2 class="counter-value" data-count="{{$data -> users}}">900</h2>
               <span>{{$data -> text_users}}</span>
@@ -15,7 +15,7 @@
           </div>
           <div class="statistics-block">
             <img
-              src="{{Voyager::image($data -> image_staff)}}"
+              src="{{Image::make(Voyager::image($data -> image_staff))}}"
               alt=""
               class="statistics-icon"
             />
